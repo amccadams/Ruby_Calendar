d09c4ede7274936c73c3e1bd64d405fe24e77077
amccadams <amccadams@gmail.com>
2013-04-29 14:40:27 -0500

    first integration tests for cal

--- calendar.rb
+++ calendar.rb
@@ -1,12 +1,17 @@
 
 class Calendar
-month =ARGV[0]
-year =ARGV[1]
+# month =ARGV[0]
+# year =ARGV[1]
 
-# puts `cal #{month} #{year}`
+attr_reader :month, :year
 
-def print_header (month, year)
-	month = [
+def initialize (month, year)
+   @month = month
+   @year = year
+end
+
+def month_name
+	month_names = [
 		"January",
 		"February",
 		"March",
@@ -20,13 +25,42 @@ def print_header (month, year)
 		"November",
 		"December"
 	]
-	year = [some variable I will make later]
+
+	month_names[month.to_i-1]
 end
 
-def next_list_item=(linked_list_item)
-    if self === linked_list_item
-      raise ArgumentError
-    end
-    @next_list_item = linked_list_item
-  end
+def month_header
+	"#{month_name} #{year}"
 end
+
+def days_of_week
+	day_names = 
+
+		"Su Mo Tu We Th Fr Sa"
+	end
+
+# def leap_year
+# 	if year is divisible by 400 then
+#    is_leap_year
+# elsif year is divisible by 100 then
+#    not_leap_year
+# elsif year is divisible by 4 then
+#    is_leap_year
+# else
+#    not_leap_year
+#  end
+
+# puts `cal #{month} #{year}`
+
+# def print_header (month, year)
+# 	
+# 	# year = [some variable I will make later]
+# end
+
+# # def next_list_item=(linked_list_item)
+# #     if self === linked_list_item
+# #       raise ArgumentError
+# #     end
+# #     @next_list_item = linked_list_item
+# #   end
+ end
--- test/test_calendar_integration.rb
+++ test/test_calendar_integration.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require './calendar'
 
 class CalendarTest < Test::Unit::TestCase
 
@@ -6,10 +7,48 @@ class CalendarTest < Test::Unit::TestCase
    #   assert_equal(`cal 2 2012`,`ruby calendar.rb 2 2012`)
    # end
 
-   def test_print_header
-     assert_equal ("January")
 
-   end
+def test_01_month_name_string
+ 	cal = Calendar.new(9, 2012)
+	assert_equal("September",cal.month_name)
+end
+
+def test_02_month_header_string
+ 	cal = Calendar.new(11, 1997)
+	assert_equal("November 1997",cal.month_header)
+end
+
+def test_03_days_of_week
+	cal = Calendar.new(2, 2012)
+	assert_equal("Su Mo Tu We Th Fr Sa",cal.days_of_week)
+end
+
+def test_04_leapyear
+	cal = Calendar.new(2012)
+	# assert_equal("2012",cal.leap_year)
+end
+# def test_2_days_of_week
+# 	Wednesday
+# end
+
+# def test_3_years
+# end
+
+# def test_4_leap_years
+# end
+
+# def test_5_first_days_of_month
+# end
+
+# def test_6_days_in_month
+# end
+
+
+
+   # def test_print_header 
+   #   header = Calendar.new("January")
+   #   assert_equal("January")
+   # end
 
 
 end
